f813111ac3a947744601a0604acfc00cb4459d34
jeongks <kisuk.jeong.lds@gmail.com>
2020-09-29 22:10:53 +0900

    added w03

---
+++ web/w03/welcome.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Wk 3, Grp 4</title>
+        <meta name="description" content="Week 3 team assignment">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+    <p>Name: <?php echo $_POST["name"]; ?></p>
+    <p>Email: <?php echo $_POST["email"]; ?></p>
+    <p>Major: <?php echo $_POST["major"]; ?></p>
+    <p>Comments: <?php echo $_POST["comments"]; ?></p>
+    <p>Continent: <?php
+
+        $continents = $_POST["continents"];
+        $count = count($continents);
+        for ($i = 0; $i < $count ; $i++){
+            echo($continents[$i]);
+        }
+
+    ?>
+        <script src="" async defer></script>
+    </body>
+</html>
+white_check_mark
+eyes
+raised_hands
+
+
+
+
+
