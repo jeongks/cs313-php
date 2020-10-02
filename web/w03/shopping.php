bdacc1234087e33ad6efe56c9759915bc9872515
jeongks <kisuk.jeong.lds@gmail.com>
2020-10-03 02:00:35 +0900

    added

--- web/w03/shopping.php
+++ web/w03/shopping.php
@@ -1,29 +1,30 @@
 <!DOCTYPE html>
 <html lang="en-us">
     <head>
-        <meta charset="utf-8"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <title>shopping cart</title>
-
+    <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>w03 prove</title>
+        <meta name="description" content="Week 3 prove assignment">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
     </head>
     <body>
         <main>
+            <h2><a href="cart.php">go to shopping cart</a></h2>
+
             <form action="cart.php" method="post">
                 <div>
-                    <input type="checkbox" name="shopinglistp[]" id="gamming_keyboard"/>
+                    <input type="checkbox" name="shopinglist[]" id="gamming_keyboard"/>
                     <label for="gamming_keyboard">Gamming Keyboard</label>
-                    <input type="checkbox" name="shopinglistp[]" id="gamming_mouse"/>
+                    <input type="checkbox" name="shopinglist[]" id="gamming_mouse"/>
                     <label for="gamming_mouse">Gamming Mouse</label>
-                    <input type="checkbox" name="shopinglistp[]" id="gamming_laptop"/>
+                    <input type="checkbox" name="shopinglist[]" id="gamming_laptop"/>
                     <label for="gamming_laptop">Gamming Laptop</label>
-                    <input type="checkbox" name="shopinglistp[]" id="gamming_desktop"/>
+                    <input type="checkbox" name="shopinglist[]" id="gamming_desktop"/>
                     <label for="gamming_desktop">Gamming Desktop</label>
-                    <input type="checkbox" name="shopinglistp[]" id="gamming_monitor"/>
+                    <input type="checkbox" name="shopinglist[]" id="gamming_monitor"/>
                     <label for="gamming_monitor">Gamming Monitor</label>
-                    <input type="checkbox" name="shopinglistp[]" id="chining_dipping"/>
-                    <label for="chining_dipping">Chining Dipping</label>
-                    <input type="checkbox" name="shopinglistp[]" id="dumbbell"/>
-                    <label for="dumbbell">80kg sets</label>
+                   
                 </div>  
                 <input type="submit" value="Add to Shopping cart">
         
