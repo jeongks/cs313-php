d3f2cbff9e796158142aaedf8045720c40ff5114
jeongks <kisuk.jeong.lds@gmail.com>
2020-10-03 01:07:53 +0900

    w03 prove stat

---
+++ web/w03/shopping.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en-us">
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <title>shopping cart</title>
+
+    </head>
+    <body>
+        <main>
+            <form action="cart.php" method="post">
+                <div>
+                    <input type="checkbox" name="shopinglistp[]" id="gamming_keyboard"/>
+                    <label for="gamming_keyboard">Gamming Keyboard</label>
+                    <input type="checkbox" name="shopinglistp[]" id="gamming_mouse"/>
+                    <label for="gamming_mouse">Gamming Mouse</label>
+                    <input type="checkbox" name="shopinglistp[]" id="gamming_laptop"/>
+                    <label for="gamming_laptop">Gamming Laptop</label>
+                    <input type="checkbox" name="shopinglistp[]" id="gamming_desktop"/>
+                    <label for="gamming_desktop">Gamming Desktop</label>
+                    <input type="checkbox" name="shopinglistp[]" id="gamming_monitor"/>
+                    <label for="gamming_monitor">Gamming Monitor</label>
+                    <input type="checkbox" name="shopinglistp[]" id="chining_dipping"/>
+                    <label for="chining_dipping">Chining Dipping</label>
+                    <input type="checkbox" name="shopinglistp[]" id="dumbbell"/>
+                    <label for="dumbbell">80kg sets</label>
+                </div>  
+                <input type="submit" value="Add to Shopping cart">
+        
+        </main>
+       
+    </body>
+</html>
