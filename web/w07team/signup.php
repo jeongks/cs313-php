93465d9a147617761b8fd7152aff0ff0d07570e8
jeongks <kisuk.jeong.lds@gmail.com>
2020-10-27 22:03:25 +0900

    added

--- web/w07team/signup.php
+++ web/w07team/signup.php
@@ -1,3 +1,24 @@
+<?php
+    try{
+        $dbUrl = getenv('DATABASE_URL');
+        
+        $dbOpts = parse_url($dbUrl);
+        
+        $dbHost = $dbOpts["host"];
+        $dbPort = $dbOpts["port"];
+        $dbUser = $dbOpts["user"];
+        $dbPassword = $dbOpts["pass"];
+        $dbName = ltrim($dbOpts["path"],'/');
+        
+        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+        
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch (PDOException $ex){
+        echo 'Error!: ' . $ex->getMessage();
+        die();
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,6 +35,7 @@
         <input type="submit" id="submit" name="submit" value="sign in"/>
     </form>
     <?php
+        
         $username = filter_input(INPUT_POST,"username", FILTER_SANITIZE_STRING);
         $password = filter_input(INPUT_POST, "password" , FILTER_SANITIZE_STRING);
         
@@ -45,14 +67,13 @@
 
         $db = dbconnect();
 
-        $sql = "INSERT INTO login (username, password) VALUES (:username, :password)"
+        $sql = "INSERT INTO login (username, password) VALUES (:username, :password)";
         $stmt = $db -> prepare($sql);
 
-        $stmt -> bindValue(:username, $username);
-        $stmt -> bindValue(:password, $passwordHash);
+        $stmt -> bindValue(':username', $username, PDO::PARAM_STR);
+        $stmt -> bindValue(':password', $passwordHash, PDO::PARAM_STR);
 
         $stmt -> execute();
-        
 
     ?>
     
