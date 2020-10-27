a7f95cb625a2332f29e74be4b8257dd08b498f23
jeongks <kisuk.jeong.lds@gmail.com>
2020-10-27 22:08:06 +0900

    added

--- web/w07team/signup.php
+++ web/w07team/signup.php
@@ -18,6 +18,29 @@
         echo 'Error!: ' . $ex->getMessage();
         die();
     }
+    function dbconnect() {
+        $db = NULL;
+        try{
+            $dbUrl = getenv('DATABASE_URL');
+            
+            $dbOpts = parse_url($dbUrl);
+            
+            $dbHost = $dbOpts["host"];
+            $dbPort = $dbOpts["port"];
+            $dbUser = $dbOpts["user"];
+            $dbPassword = $dbOpts["pass"];
+            $dbName = ltrim($dbOpts["path"],'/');
+            
+            $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+            
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch (PDOException $ex){
+            echo 'Error!: ' . $ex->getMessage();
+            die();
+        }
+        return $db;
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -35,46 +58,23 @@
         <input type="submit" id="submit" name="submit" value="sign in"/>
     </form>
     <?php
-        
-        $username = filter_input(INPUT_POST,"username", FILTER_SANITIZE_STRING);
-        $password = filter_input(INPUT_POST, "password" , FILTER_SANITIZE_STRING);
-        
-        $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+        if(isset($_POST['submit'])){
+            $username = filter_input(INPUT_POST,"username", FILTER_SANITIZE_STRING);
+            $password = filter_input(INPUT_POST, "password" , FILTER_SANITIZE_STRING);
+            
+            $passwordHash = password_hash($password, PASSWORD_DEFAULT);
 
-        function dbconnect() {
-            $db = NULL;
-            try{
-                $dbUrl = getenv('DATABASE_URL');
-                
-                $dbOpts = parse_url($dbUrl);
-                
-                $dbHost = $dbOpts["host"];
-                $dbPort = $dbOpts["port"];
-                $dbUser = $dbOpts["user"];
-                $dbPassword = $dbOpts["pass"];
-                $dbName = ltrim($dbOpts["path"],'/');
-                
-                $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-                
-                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            }
-            catch (PDOException $ex){
-                echo 'Error!: ' . $ex->getMessage();
-                die();
-            }
-            return $db;
+            $db = dbconnect();
+
+            $sql = "INSERT INTO login (username, password) VALUES (:username, :password)";
+            $stmt = $db -> prepare($sql);
+
+            $stmt -> bindValue(':username', $username, PDO::PARAM_STR);
+            $stmt -> bindValue(':password', $passwordHash, PDO::PARAM_STR);
+
+            $stmt -> execute();
         }
-
-        $db = dbconnect();
-
-        $sql = "INSERT INTO login (username, password) VALUES (:username, :password)";
-        $stmt = $db -> prepare($sql);
-
-        $stmt -> bindValue(':username', $username, PDO::PARAM_STR);
-        $stmt -> bindValue(':password', $passwordHash, PDO::PARAM_STR);
-
-        $stmt -> execute();
-
+        
     ?>
     
 </body>
