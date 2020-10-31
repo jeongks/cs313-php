50f5f899c8e6c61ca37acb52f3b97f93d6e0dd80
jeongks <kisuk.jeong.lds@gmail.com>
2020-10-31 23:40:20 +0900

    edited

--- web/w05/index.php
+++ web/w05/index.php
@@ -1,3 +1,23 @@
+<?php
+  try{
+    $dbUrl = getenv('DATABASE_URL');
+    
+    $dbOpts = parse_url($dbUrl);
+    
+    $dbHost = $dbOpts["host"];
+    $dbPort = $dbOpts["port"];
+    $dbUser = $dbOpts["user"];
+    $dbPassword = $dbOpts["pass"];
+    $dbName = ltrim($dbOpts["path"],'/');
+    
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  } catch (PDOException $ex){
+    echo 'Error!: ' . $ex->getMessage();
+    die();
+  }
+?>
 <!DOCTYPE html>
 <html lang="en-us">
     <head>
@@ -28,27 +48,7 @@
           <input type="hidden" name="action" value="select-weapon"/>
 
           <?php
-            try
-            {
-            $dbUrl = getenv('DATABASE_URL');
             
-            $dbOpts = parse_url($dbUrl);
-            
-            $dbHost = $dbOpts["host"];
-            $dbPort = $dbOpts["port"];
-            $dbUser = $dbOpts["user"];
-            $dbPassword = $dbOpts["pass"];
-            $dbName = ltrim($dbOpts["path"],'/');
-            
-            $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-            
-            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            }
-            catch (PDOException $ex)
-            {
-              echo 'Error!: ' . $ex->getMessage();
-              die();
-            }
             $action = filter_input(INPUT_POST, 'action',FILTER_SANITIZE_STRING);
             if ($action == NULL){
               $action = filter_input(INPUT_GET, 'action',FILTER_SANITIZE_STRING);
