df0482e50dff3d1c902e8d7e93793308c45964cd
jeongks <kisuk.jeong.lds@gmail.com>
2020-11-01 08:58:16 +0900

    edit

--- web/w05/index.php
+++ web/w05/index.php
@@ -90,6 +90,7 @@
               $weapon_info .= '<input type="submit" name="submit" value="set weapon">'
               $weapon_info .= '<input type="hidden" name="action" value="setWeapon">'
             break;
+          }
         ?>
            
         <div class="weaponInfo">
@@ -111,118 +112,118 @@
           </form>
           
         </div>
+        <?php  
+          //   case "setWeapon":
+          //     $weaponName = filter_input(INPUT_POST, 'weaponName');
+          //     $min_damage = filter_input(INPUT_POST, 'min_damage');
+          //     $max_damage = filter_input(INPUT_POST, 'max_damage');
+          //     $weight = filter_input(INPUT_POST, 'weight');
+          //     $range = filter_input(INPUT_POST, 'range');
+          //     $durability = filter_input(INPUT_POST, 'durability');
+          //     $character_id = filter_input(INPUT_POST, 'character_id');
+              
+          //     if(empty(weaponName) || empty(min_damage) || empty(max_damage) ||empty(weight) ||empty(range) || empty(durability) || empty(character_id)){
+          //       $message ='<p>Please enter weapon information.</p>';
+          //       include 'index.php';
+          //       exit;
+          //     }
+
+          //     $sql = 'INSERT INTO weapon (name, min_damage, max_damage, weight, range, durability, rank, character_id) VALUE (:name, :min_damage,:max_damagen,:weight,:range,:durability,:rank,:character_id)';
+
+          //     $stmt = $db -> prepare($sql);
+          //     $stmt -> bindValue(':name',$weaponName,PDO::PARAM_STR);
+          //     $stmt -> bindValue(':min_damage',$min_damage,PDO::PARAM_STR);
+          //     $stmt -> bindValue(':max_damage',$max_damage,PDO::PARAM_STR);
+          //     $stmt -> bindValue(':weight',$weight,PDO::PARAM_STR);
+          //     $stmt -> bindValue(':range',$range,PDO::PARAM_STR);
+          //     $stmt -> bindValue(':durability',$durabiilty,PDO::PARAM_STR);
+          //     $stmt -> bindValue(':rank',$rank,PDO::PARAM_STR);
+          //     $stmt -> bindValue(':chracter_id',$character_id,PDO::PARAM_STR);
+              
+          //     $stmt -> execute();
+
+          //     $statement = $db ->query('SELECT * FROM weapon');
+          //     while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+          //     {
+          //         $weaponname = $row['name'];
+          //         $mindamage = $row['min_damage'];
+          //         $maxdamage = $row['max-damage'];
+          //         $weaponweight = $row['weight'];
+          //         $weaponrange = $row['range'];
+          //         $weapondurability = $row['durability'];
+          //         $weaponrank = $row['rank'];
+          //         $characterid = $row['character_id'];
+
+          //         $weaponlist = "<tr>";
+          //         $weaponlist .= "<td>$weaponname<td>";
+          //         $weaponlist .= "<td>$mindamage<td>";
+          //         $weaponlist .= "<td>$maxdamage<td>";
+          //         $weaponlist .= "<td>$weaponweight<td>";
+          //         $weaponlist .= "<td>$weaponrange<td>";
+          //         $weaponlist .= "<td>$weapondurability<td>";
+          //         $weaponlist .= "<td>$weaponrank<td>";
+          //         $weaponlist .= "<td>$characterid<td>";
+          //         $weaponlist .= "</tr>";
+                  
+          //     }
+          //   default:
+          //     include 'view/home.php';   
+          // }
+
+
+
+
+          // }
+            
+          
         
+
+
+
+
+        
+          //need to have function to create <td>
+
+
+          // echo $weaponlist;
+        
+        
+
+          // try
+          // {
+          //   $weapon = $_POST['weapons'];
+          //   $user = 'postgres';
+          //   $password = 'password';
+          //   $db = new PDO('pgsql:host=localhost;dbname=mydb', $user, $password);
+
+          //   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          // }
+          // catch (PDOException $ex)
+          // {
+          //   echo 'ERROR!: ' . $ex->getMessage();
+          //   die();
+          // }
+
+          // $stmt = $db->prepare('SELECT * FROM Sword s INNER JOIN Weapon w ON s.weapon_id = w.id INNER JOIN Option o ON s.option_id = o.id WHERE weapons = :weapons');
+          // $stmt->bindValue(':weapons', $weapon, PDO::PARAM_STR);
+          // $stmt->execute();
+          // $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+          // foreach ($rows as $row)
+          // {
+          //   echo '<p>' . $row['name'] . ' ' . $row['two_handed'] . ' ' . $row['min_damage'] . ' ' . $row['max_damage'].' '. $row['weight']. ' '.$row['range']. ' ' . $row['durability']. ' '.$row['rank']. ' ' . $row['value']  . '</p>';
+
+          // }
+
+
+
+      ?>
         
     </body>
 </html>
       
           
             
-  <?php  
-    //   case "setWeapon":
-    //     $weaponName = filter_input(INPUT_POST, 'weaponName');
-    //     $min_damage = filter_input(INPUT_POST, 'min_damage');
-    //     $max_damage = filter_input(INPUT_POST, 'max_damage');
-    //     $weight = filter_input(INPUT_POST, 'weight');
-    //     $range = filter_input(INPUT_POST, 'range');
-    //     $durability = filter_input(INPUT_POST, 'durability');
-    //     $character_id = filter_input(INPUT_POST, 'character_id');
-        
-    //     if(empty(weaponName) || empty(min_damage) || empty(max_damage) ||empty(weight) ||empty(range) || empty(durability) || empty(character_id)){
-    //       $message ='<p>Please enter weapon information.</p>';
-    //       include 'index.php';
-    //       exit;
-    //     }
-
-    //     $sql = 'INSERT INTO weapon (name, min_damage, max_damage, weight, range, durability, rank, character_id) VALUE (:name, :min_damage,:max_damagen,:weight,:range,:durability,:rank,:character_id)';
-
-    //     $stmt = $db -> prepare($sql);
-    //     $stmt -> bindValue(':name',$weaponName,PDO::PARAM_STR);
-    //     $stmt -> bindValue(':min_damage',$min_damage,PDO::PARAM_STR);
-    //     $stmt -> bindValue(':max_damage',$max_damage,PDO::PARAM_STR);
-    //     $stmt -> bindValue(':weight',$weight,PDO::PARAM_STR);
-    //     $stmt -> bindValue(':range',$range,PDO::PARAM_STR);
-    //     $stmt -> bindValue(':durability',$durabiilty,PDO::PARAM_STR);
-    //     $stmt -> bindValue(':rank',$rank,PDO::PARAM_STR);
-    //     $stmt -> bindValue(':chracter_id',$character_id,PDO::PARAM_STR);
-        
-    //     $stmt -> execute();
-
-    //     $statement = $db ->query('SELECT * FROM weapon');
-    //     while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-    //     {
-    //         $weaponname = $row['name'];
-    //         $mindamage = $row['min_damage'];
-    //         $maxdamage = $row['max-damage'];
-    //         $weaponweight = $row['weight'];
-    //         $weaponrange = $row['range'];
-    //         $weapondurability = $row['durability'];
-    //         $weaponrank = $row['rank'];
-    //         $characterid = $row['character_id'];
-
-    //         $weaponlist = "<tr>";
-    //         $weaponlist .= "<td>$weaponname<td>";
-    //         $weaponlist .= "<td>$mindamage<td>";
-    //         $weaponlist .= "<td>$maxdamage<td>";
-    //         $weaponlist .= "<td>$weaponweight<td>";
-    //         $weaponlist .= "<td>$weaponrange<td>";
-    //         $weaponlist .= "<td>$weapondurability<td>";
-    //         $weaponlist .= "<td>$weaponrank<td>";
-    //         $weaponlist .= "<td>$characterid<td>";
-    //         $weaponlist .= "</tr>";
-            
-    //     }
-    //   default:
-    //     include 'view/home.php';   
-    // }
-
-
-
-
-    // }
-      
-    
   
-
-
-
-
-  
-    //need to have function to create <td>
-
-
-    // echo $weaponlist;
-  
-  
-
-  // try
-  // {
-  //   $weapon = $_POST['weapons'];
-  //   $user = 'postgres';
-  //   $password = 'password';
-  //   $db = new PDO('pgsql:host=localhost;dbname=mydb', $user, $password);
-
-  //   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  // }
-  // catch (PDOException $ex)
-  // {
-  //   echo 'ERROR!: ' . $ex->getMessage();
-  //   die();
-  // }
-
-  // $stmt = $db->prepare('SELECT * FROM Sword s INNER JOIN Weapon w ON s.weapon_id = w.id INNER JOIN Option o ON s.option_id = o.id WHERE weapons = :weapons');
-  // $stmt->bindValue(':weapons', $weapon, PDO::PARAM_STR);
-  // $stmt->execute();
-  // $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-  // foreach ($rows as $row)
-  // {
-  //   echo '<p>' . $row['name'] . ' ' . $row['two_handed'] . ' ' . $row['min_damage'] . ' ' . $row['max_damage'].' '. $row['weight']. ' '.$row['range']. ' ' . $row['durability']. ' '.$row['rank']. ' ' . $row['value']  . '</p>';
-
-  // }
-
-
-
-?>
         
     
