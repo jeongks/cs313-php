1e7fce5699e645e2f8fcf4db8f4b263bd2c85597
jeongks <kisuk.jeong.lds@gmail.com>
2020-10-31 23:52:31 +0900

    added

--- web/w05/index.php
+++ web/w05/index.php
@@ -1,22 +1,22 @@
 <?php
-  // try{
-  //   $dbUrl = getenv('DATABASE_URL');
+  try{
+    $dbUrl = getenv('DATABASE_URL');
     
-  //   $dbOpts = parse_url($dbUrl);
+    $dbOpts = parse_url($dbUrl);
     
-  //   $dbHost = $dbOpts["host"];
-  //   $dbPort = $dbOpts["port"];
-  //   $dbUser = $dbOpts["user"];
-  //   $dbPassword = $dbOpts["pass"];
-  //   $dbName = ltrim($dbOpts["path"],'/');
+    $dbHost = $dbOpts["host"];
+    $dbPort = $dbOpts["port"];
+    $dbUser = $dbOpts["user"];
+    $dbPassword = $dbOpts["pass"];
+    $dbName = ltrim($dbOpts["path"],'/');
     
-  //   $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
     
-  //   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  // } catch (PDOException $ex){
-  //   echo 'Error!: ' . $ex->getMessage();
-  //   die();
-  // }
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  } catch (PDOException $ex){
+    echo 'Error!: ' . $ex->getMessage();
+    die();
+  }
 ?>
 <!DOCTYPE html>
 <html lang="en-us">
@@ -182,6 +182,9 @@
           <th>option</th>
         </tr>
         <?php
+          //need to have function to create <td>
+
+
           // echo $weaponlist;
         ?>
         <table>
